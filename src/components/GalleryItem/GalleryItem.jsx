42010c8e05e9be98a6d2b3487639bb8bae904ae8
Bryce Morries <brycemorries@MacBook-Pro.localdomain>
2023-05-01 19:35:19 -0500

    added the conditional rendering on image click to shwo the description

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 function GalleryItem(props) {
     const [like, setLikes] = useState([props.likes]);
+    const [status, setStatus] = useState([false]);
+
     const likeButton = () => {
         axios({
             method: 'PUT',
@@ -12,10 +14,38 @@ function GalleryItem(props) {
         })
     }
 
+    const picScript = () => {
+        if (!status) {
+            setStatus(true)
+        } else {
+            setStatus(false)
+        }
+    }
+
+    const showElement = () => {
+        if (!status) {
+            return (
+                <p className='descript'>{props.description}</p>
+
+            )
+        } else {
+            return (
+                <img
+                    src={props.path}
+                    alt=""
+                    onClick={picScript}
+                />
+            );
+        }
+    }
+
+
     return (
         <div className='box' >
             <div key={props.id}>
-                <img src={props.path} alt="" />
+                <div onClick={picScript}>
+                    {showElement()}
+                </div>
                 <p>This has {like} likes.</p>
                 <button onClick={likeButton}>Like</button>
             </div>
